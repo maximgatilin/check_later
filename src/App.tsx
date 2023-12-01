6e02cdf731e2cb4d86638e5b8424bcff6b520c72
Maksim Gatilin <maxim@supportlogic.io>
2023-12-01 16:26:58 +0300

    Show tabs & content based on selected tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import { useState, } from 'react';
+import { useState, useMemo } from 'react';
 import './App.css';
 
 function App() {
   const [items, setItems] = useState([
-    {id: 0, name: 'Отцы и дети Тургенева', type: 'book'}
+    {id: guid(), name: 'Отцы и дети Тургенева', action_type: 'read'},
+    {id: guid(), name: 'Терминатор 2', action_type: 'watch'}
   ]);
 
   const [isEditorVisible, setIsEditorVisible] = useState(false);
   const [editorText, setEditorText] = useState('');
+  const [selectedTab, setSelectedTab] = useState('watch');
+  const itemsFiltered = useMemo(() => {
+    return items.filter((i) => i.action_type === selectedTab);
+  }, [items, selectedTab]);
+
+  const tabs = [
+    { label: 'Посмотреть', value: 'watch' },
+    { label: 'Почитать', value: 'read' },
+  ];
 
   return (
     <div>
-      {items.map((i) => (<div style={{ display: 'inline-block', border: '1px solid black', margin: 10, padding: 5 }} key={i.id}>{i.name}</div>))}
+      <div>
+        {tabs.map(t => (
+          <button key={t.value} style={{ border: t.value === selectedTab ? '1px solid red' : undefined }} onClick={() => setSelectedTab(t.value)}>{t.label}</button>
+        ))}
+      </div>
+      <div>
+        {itemsFiltered.map((i) => (<div style={{ display: 'inline-block', border: '1px solid black', margin: 10, padding: 5 }} key={i.id}>{i.name}</div>))}
+      </div>
       <button onClick={() => {
         setIsEditorVisible(true);
       }}>Добавить</button>
@@ -21,7 +38,7 @@ function App() {
               if (e.key === 'Enter') {
                 setItems([
                   ...items,
-                  {id: items.length + 1, name: editorText, type: 'book'}
+                  {id: guid(), name: editorText, action_type: selectedTab}
                 ]);
                 setEditorText('');
                 setIsEditorVisible(false);
@@ -31,6 +48,18 @@ function App() {
       )}
     </div>
   );
+}
+
+// generates random id;
+// of course it's better to use uuid library, but decided this way is good enough for pet project
+let guid = () => {
+  let s4 = () => {
+      return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+  }
+  // return id of format 'aaaaaaaa'-'aaaa'-'aaaa'-'aaaa'-'aaaaaaaaaaaa'
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
 export default App;
