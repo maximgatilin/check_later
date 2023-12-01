94c0a3942e03dfe947d4e1f5f064ce3ef6f75f18
Maksim Gatilin <maxim@supportlogic.io>
2023-12-01 16:08:58 +0300

    Added basic logic & jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, } from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([
+    {id: 0, name: 'Отцы и дети Тургенева', type: 'book'}
+  ]);
+
+  const [isEditorVisible, setIsEditorVisible] = useState(false);
+  const [editorText, setEditorText] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {items.map((i) => (<div style={{ display: 'inline-block', border: '1px solid black', margin: 10, padding: 5 }} key={i.id}>{i.name}</div>))}
+      <button onClick={() => {
+        setIsEditorVisible(true);
+      }}>Добавить</button>
+      {isEditorVisible && (
+        <div style={{ display: 'inline-block', border: '1px solid black', margin: 10, padding: 5 }}>
+          <input type="text" value={editorText} onChange={e => setEditorText(e.target.value)} onKeyUp={(e) => {
+              if (e.key === 'Enter') {
+                setItems([
+                  ...items,
+                  {id: items.length + 1, name: editorText, type: 'book'}
+                ]);
+                setEditorText('');
+                setIsEditorVisible(false);
+              }
+          }} />
+        </div>
+      )}
     </div>
   );
 }
